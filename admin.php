86a322e6a730e945cfe3aedc6e968dfbe0432149
Алексей <zhdankinaleksey@gmail.com>
2019-05-14 18:52:54 +0700

    14.05. 18:52

--- admin.php
+++ admin.php
@@ -1,35 +1,25 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Вход в систему</title>
-  <style>
-    body {
-      margin: 20px;
-    }
-    * {
-      font-size: 30px;
-    }
-    button {
-      margin-top: 20px;
-    }
-  </style>
-</head>
-<body>
-  <?php
-    session_start();
-    if (!$_SESSION['login'] || !$_SESSION['password']) {
-      header(string: 'Location: login.php');
-      die();
-    }
-
-    if ($_POST['unlogin']) {
-      session_destroy();
-      header(string: 'Location: login.php')
-    }
-  ?>
+<?php
+session_start();
+if (!$_SESSION['login'] || !$_SESSION['password']) {
+  header('Location: login.php');
+  die();
+}
+if ($_POST['unlogin']) {
+  session_destroy();
+  header('Location: login.php');
+}
+?>
+<style>
+  body {
+    margin: 20px;
+  }
+  * {
+    font-size: 30px;
+  }
+  button {
+    margin-top: 20px;
+  }
+</style>
 
   <h1>Форма администратора</h1>
 
@@ -38,6 +28,3 @@
   <form method="POST">
     <input type="submit" name="unlogin" value="Выйти из формы администратора">
   </form>
-
-</body>
-</html>
--- login.php
+++ login.php
@@ -1,44 +1,39 @@
-<!DOCTYPE html>
-<html lang="ru">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Вход в систему</title>
-  <style>
-    body {
-      margin: 50% auto;
-    }
-    input, textarea, button {
-      margin: 15px;
-      display: block;
-      font-size: 30px;
-    }
-  </style>
-</head>
-<body>
-
 <?php
-  session_start();
-  $conection = new PDO (dsn: 'mysql:host=localhost; dbname=shop charset=utf-8', username: 'root', passw: '');
-  $data = $conection->query(statement: 'SELECT * FROM `admin`');
-  if ($_POST['login']) {
-    foreach ($data as $info) {
-      if ($_POST['login'] == $info['login'] && $_POST['password'] == $info['password']) {
-        $_SESSION['login'] = $_POST['login'];
-        $_SESSION['password'] = $_POST['password'];
-        header (string: 'Location: admin.php')
-      }
+session_start();
+$connection = new PDO ('mysql: host=localhost; dbname=shop; charset=utf8', 'root', '');
+$data = $connection->query('SELECT * FROM `admin`');
+if ($_POST['login']) {
+  foreach ($data as $info) {
+    if ($_POST['login'] == $info['login'] && $_POST['password'] == $info['password']) {
+    $_SESSION['login'] = $_POST['login'];
+    $_SESSION['password'] = $_POST['password'];
+    header ('Location: admin.php');
     }
   }
+}
 ?>
-
-  <h2>Вход в систему</h2>
-  <form method="POST">
-    <input type="text" name="login" placeholder="Логин" required>
-    <input type="password" name="password" placeholder="Пароль" required>
-    <button>Отправить</button>
-  </form>
-
-</body>
-</html>
+<style>
+  body {
+    background-color: #EDEDED
+  }
+  input, textarea, button {
+    margin: 15px;
+    display: block;
+    font-size: 30px;
+  }
+  input {
+    color: #1B1464;
+    background-color: #dbdbdb;
+  }
+  button {
+    padding: 10px;
+    color: #1B1464;
+    background-color: #FBB03B;
+  }
+</style>
+<h2>Вход в систему</h2>
+<form method="POST">
+  <input type="text" name="login" placeholder="Логин" required>
+  <input type="password" name="password" placeholder="Пароль" required>
+  <button>Отправить</button>
+</form>
